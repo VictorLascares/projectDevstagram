41525b17a6b6abf250ab910753fd4149d6c43242
Victor Lascares <vmlascares@gmail.com>
2022-06-10 22:22:08 -0500

    Creando el template de registro

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('principal');
+  return view('principal');
+});
+Route::get('/crear-cuenta', function () {
+  return view('auth.register');
 });
