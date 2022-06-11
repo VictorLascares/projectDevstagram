5c0c6c02eea8e2be8c60d8f196b1242a404d4aca
Victor Lascares <vmlascares@gmail.com>
2022-06-10 22:42:30 -0500

    Agregando el controlador de registro

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
   return view('principal');
 });
-Route::get('/crear-cuenta', function () {
-  return view('auth.register');
-});
+Route::get('/crear-cuenta', [RegisterController::class, 'index']);
