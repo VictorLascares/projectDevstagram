c7f1c962409d0259838077e0d200d9479cf23492
Victor Lascares <vmlascares@gmail.com>
2022-06-12 18:43:01 -0500

    Nombrando ruta

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::get('/', function () {
   return view('principal');
 });
-Route::get('/crear-cuenta', [RegisterController::class, 'index']);
-Route::post('/crear-cuenta', [RegisterController::class, 'store']);
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
