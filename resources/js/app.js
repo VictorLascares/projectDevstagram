0b8b724e368a0bad7a3ace7915a5d19611f0d8db
Victor Lascares <vmlascares@gmail.com>
2022-06-13 16:26:19 -0500

    Instalando dropzone

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,13 @@
-import './bootstrap';
+import Dropzone from 'dropzone';
 
+
+Dropzone.autoDiscover = false;
+const dropzone = new Dropzone('#dropzone',{
+    dicDefaultMessage: 'Sube aqui tu Imagen',
+    acceptedFiles: '.png, .jpg, .jpeg, .gif',
+    addRemoveLinks: true,
+    dicrRemoveFile: 'Borrar Archivo',
+    maxFiles: 1,
+    uploadMultiple: false,
+})
 
