0f60540220387c315625f2c185b09a195d8001ac
Victor Lascares <vmlascares@gmail.com>
2022-06-13 14:25:36 -0500

    Añadiendo el resto de información

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,26 @@
 @extends('layouts.app')
 @section('titulo')
-    Tu cuenta
+    Perfil: {{ $user->username }}
 @endsection
 @section('contenido')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col md:flex-row items-center">
+            <div class="w-8/12 px-5">
                 <img src="{{ asset('img/usuario.svg') }}" alt="Imagen de Usuario">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+            <div class="flex flex-col items-center md:block md:w-8/12 lg:w-6/12 px-5">
+                <p class="text-gray-700 text-2xl mb-3">{{ $user->username }}</p>
+                <div class="md:flex md:justify-between md:items-center gap-2">
+                    <p class="flex justify-start gap-1 text-gray-800 text-sm mb-3 md:mb-0 font-bold">
+                        0<span class="font-normal">Publicaciones</span>
+                    </p>
+                    <p class="flex justify-start gap-1 text-gray-800 text-sm mb-3 md:mb-0 font-bold">
+                        0<span class="font-normal">Seguidores</span>
+                    </p>
+                    <p class="flex justify-start gap-1 text-gray-800 text-sm mb-3 md:mb-0 font-bold">
+                        0<span class="font-normal">Seguidos</span>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
