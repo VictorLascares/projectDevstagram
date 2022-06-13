c08e0e9a732d28afd047a4367509b75e67e42930
Victor Lascares <vmlascares@gmail.com>
2022-06-13 16:55:45 -0500

    Leyendo las imagenes del request

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class ImagenController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        
+        $imagen = $request->file('file');
+
+        return response()->json(['imagen' => $imagen->extension()]);
     }
 }
