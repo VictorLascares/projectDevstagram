3f1da759b4ae9211a37786bc986d8c7a34e29074
Victor Lascares <vmlascares@gmail.com>
2022-06-13 12:19:15 -0500

    Agregando funcionalidad para mantener sesion siempre abierta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,6 +44,12 @@
             <p class="text-red-600">{{ $message }}</p>
           @enderror
         </div>
+
+        <div class="mb-5">
+            <input type="checkbox" name="remember" id="remember">
+            <label for="remember" class="text-gray-500 text-sm">Mantener mi sesión abierta</label>
+        </div>
+
         <input type="submit" value="Iniciar Sesión" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
       </form>
     </div>
