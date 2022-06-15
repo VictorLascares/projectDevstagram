937ba3f32a5fb0d4f454c23bbfff45fd8d09116f
Victor Lascares <vmlascares@gmail.com>
2022-06-15 14:07:23 -0500

    Mostrando la cantidad de Likes que tiene una publicacion

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -28,9 +28,17 @@
 
             @auth
                 @if ( $post->checkLike(auth()->user()) )
-                    <svg xmlns="http://www.w3.org/2000/svg" class="my-4 h-6 w-6 fill-red-600 stroke-red-600" viewBox="0 0 24 24" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg> 
+                    <form action="{{ route('posts.likes.destroy', $post) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <div class="my-4">
+                            <button type="submit">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-red-600 stroke-red-600" viewBox="0 0 24 24" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                </svg> 
+                            </button>
+                        </div>
+                    </form>
                 @else
                     <form action="{{ route('posts.likes.store', $post) }}" method="POST">
                         @csrf
@@ -48,7 +56,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="#000" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
-                <p>0 Likes</p>
+                <p>{{$post->likes()->count()}} Likes</p>
             </div>
 
             <div>
