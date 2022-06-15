f48c3042b631df58a31b8872bb7f691e061050b6
Victor Lascares <vmlascares@gmail.com>
2022-06-15 12:30:33 -0500

    Revisando si un usuario ya dio Like

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,17 +26,25 @@
                 @endauth
             </div>
 
-            <form action="{{ route('posts.likes.store', $post) }}" method="POST">
-                @csrf
-                <div class="my-4">
-                    <button type="submit">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                        </svg>
-                    </button>
-                </div>
-            </form>
-            <div class="flex gap-2 items-center">
+            @auth
+                @if ( $post->checkLike(auth()->user()) )
+                    <svg xmlns="http://www.w3.org/2000/svg" class="my-4 h-6 w-6 fill-red-600 stroke-red-600" viewBox="0 0 24 24" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg> 
+                @else
+                    <form action="{{ route('posts.likes.store', $post) }}" method="POST">
+                        @csrf
+                        <div class="my-4">
+                            <button type="submit">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </form>
+                @endif
+            @endauth
+            <div class="flex gap-2 items-center @guest mt-4 @endguest">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="#000" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
