f49c615b5bd96986a1f9de6f8711bce654277dc2
Victor Lascares <vmlascares@gmail.com>
2022-06-16 12:54:34 -0500

    Creando la migracion para agregar imagenes al perfil del usuario y agregando nuevos campos para editar la informacion del usuario

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 use App\Models\User;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Hash;
 
 class PerfilController extends Controller
 {
@@ -26,10 +28,12 @@ class PerfilController extends Controller
 
         $this->validate($request, [
             'username' => ['required', 'unique:users,username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter,editar-perfil'],
+            'email' => ['required', 'unique:users,email,'.auth()->user()->id, 'email', 'max:60'],
+            'password' => ['required',Password::min(8)->letters()->mixedCase()->numbers()->symbols()->uncompromised()]
         ]);
 
         if ($request->imagen) {
-            $imagen = $request->file('file');
+            $imagen = $request->file('imagen');
             $nombreImagen = Str::uuid() . "." . $imagen->extension();
             $imagenServidor = Image::make($imagen);
             $imagenServidor->fit(500, 500);
@@ -40,7 +44,13 @@ class PerfilController extends Controller
 
         $usuario = User::find(auth()->user()->id);
         $usuario->username = $request->username;
-        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->email = $request->email;
+
+        if(!auth()->attempt(['email' => auth()->user()->email, 'password' => $request->current_password], $request->remember)) {
+            return back()->with('mensaje', 'Contraseña Incorrecta');
+        }
+        $usuario->password = Hash::make($request->password);
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? '';
         $usuario->save();
 
         return redirect()->route('posts.index', $usuario->username);
--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -24,6 +24,53 @@
                       <p class="text-red-600">{{ $message }}</p>
                     @enderror
                   </div>
+                  <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                      Correo Electronico
+                    </label>
+                    <input 
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="correo@example.com"
+                      class="border p-3 w-full rounded-lg @error('email') border-red-600 @enderror"
+                      value="{{ auth()->user()->email }}"
+                    >
+                    @error('email')
+                      <p class="text-red-600">{{ $message }}</p>
+                    @enderror
+                  </div>
+
+                  <div class="mb-5">
+                    <label for="current_password" class="mb-2 block uppercase text-gray-500 font-bold">
+                      Contraseña Actual
+                    </label>
+                    <input 
+                      type="password"
+                      name="current_password"
+                      id="current_password"
+                      placeholder="Contraseña actual"
+                      class="border p-3 w-full rounded-lg @if(session('mensaje')) border-red-600 @endif"
+                    >
+                    @if (session('mensaje'))
+                        <p class="text-red-600">{{ session('mensaje') }}</p>
+                    @endif
+                  </div>
+                  <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                      Nueva Contraseña
+                    </label>
+                    <input 
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Nueva contraseña"
+                      class="border p-3 w-full rounded-lg @error('password') border-red-600 @enderror"
+                    >
+                    @error('password')
+                        <p class="text-red-600">{{ $message }}</p>
+                    @enderror
+                  </div>
 
                   <div class="mb-5">
                     <label for="imagen" class="mb-2 block uppercase text-gray-500 font-bold">
