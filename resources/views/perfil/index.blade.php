4b502dd9fc1813f8e7d90fbe3f32b78bcc41eddc
Victor Lascares <vmlascares@gmail.com>
2022-06-15 20:26:31 -0500

    Creando el formulario para editar perfil

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -3,5 +3,41 @@
     Editar Perfil:  {{ auth()->user()->username }}
 @endsection
 @section('contenido')
-    
+    <div class="md:flex md:justify-center px-4">
+        <div class="md:w-1/2 bg-white shadow p-6">
+            <form action="" class="mt-10 md:mt-0">
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                      Username
+                    </label>
+                    <input 
+                      type="text"
+                      name="username"
+                      id="username"
+                      placeholder="Nombre de usuario"
+                      class="border p-3 w-full rounded-lg @error('username') border-red-600 @enderror"
+                      value="{{ auth()->user()->username }}"
+                    >
+                    @error('username')
+                      <p class="text-red-600">{{ $message }}</p>
+                    @enderror
+                  </div>
+
+                  <div class="mb-5">
+                    <label for="imagen" class="mb-2 block uppercase text-gray-500 font-bold">
+                      Imagen Perfil
+                    </label>
+                    <input 
+                      type="file"
+                      name="imagen"
+                      id="imagen"
+                      class="border p-3 w-full rounded-lg"
+                      accept=".jpg, .jpeg, .png"
+                    >
+                  </div>
+
+                  <input type="submit" value="Guardar cambios" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
 @endsection
