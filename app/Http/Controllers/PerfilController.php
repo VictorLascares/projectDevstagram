160554ae621feb40dea43da7ea7038413b52bcac
Victor Lascares <vmlascares@gmail.com>
2022-06-16 11:28:41 -0500

    Almacenando la informacion del usuario

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
+use App\Models\User;
 
 class PerfilController extends Controller
 {
@@ -25,5 +27,22 @@ class PerfilController extends Controller
         $this->validate($request, [
             'username' => ['required', 'unique:users,username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter,editar-perfil'],
         ]);
+
+        if ($request->imagen) {
+            $imagen = $request->file('file');
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+            $imagenServidor = Image::make($imagen);
+            $imagenServidor->fit(500, 500);
+
+            $imagenPath = public_path('img/perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+
+        return redirect()->route('posts.index', $usuario->username);
     }
 }
